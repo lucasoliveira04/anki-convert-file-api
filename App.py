b1572c1a09836593b9458bbd391aef3370a41b4a
= <camposdlucasoli@gmail.com>
2025-03-24 14:42:27 -0300

    Primeira versão da API

--- App.py
+++ App.py
@@ -60,5 +60,5 @@ def convert_to_apkg():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    # Rodando o servidor Flask na porta 5000
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
